c78f4cecc4a1084fe8919b4fe0892f85f0b50c8b
zyclincoln <zyclincoln@gmail.com>
2020-07-29 12:12:45 +0800

    update boundary shape

--- BallBoundary.cpp
+++ BallBoundary.cpp
@@ -9,7 +9,9 @@ void BallBoundary::apply_density(std::vector<Field<double, 1> > &d) {
     for(int i = 0; i < 3; ++i){
         for(int y = max(1, y_-r_); y <= min(d[i].get_height()-2, y_+r_); ++y){
             for(int x = max(1, x_-r_); x <= min(d[i].get_width()-2, x_+r_); ++x){
-                d[i](x, y, 0) += rgb[i] * 0.1;
+                if ((x - x_) * (x - x_) + (y - y_) * (y - y_) <= r_ * r_) {
+                    d[i](x, y, 0) += rgb[i] * 0.1;
+                }
             }
         }
     }
@@ -18,8 +20,10 @@ void BallBoundary::apply_density(std::vector<Field<double, 1> > &d) {
 void BallBoundary::apply_velocity(Field<double, 2> &v) {
     for(int y = max(1, y_-r_); y <= min(v.get_height()-2, y_+r_); ++y){
         for(int x = max(1, x_-r_); x <= min(v.get_width()-2, x_+r_); ++x){
-            v(x, y, 0) += dx_ * 0.05;
-            v(x, y, 1) += dy_ * 0.05;
+            if ((x - x_) * (x - x_) + (y - y_) * (y - y_) <= r_ * r_) {
+                v(x, y, 0) += dx_ * 0.05;
+                v(x, y, 1) += dy_ * 0.05;
+            }
         }
     }
 }
